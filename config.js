a9327889064d70a4cda94eff244226332b991585
vijay <vijaym@10decoders.in>
2020-12-07 14:41:43 +0530

    new VoiceIt key added

--- config.js
+++ config.js
@@ -4,8 +4,14 @@ const config = {
   // contentLanguage: process.env.CONTENT_LANGUAGE| "en-US",
   // chosenVoicePrintPhrase: process.env.VOICEPRINT_PHRASE|"Hello I am very happy today",
   // dataBaseURL:  process.env.DATABASE_URL | "mongodb://localhost:27019/localmachine"
-  apiKey :"key_0e1554454e0f48479255a7a5be76bc74",
-  apiToken :"tok_4740ac55f56e43f2ba2f302b138fcf81",
+  
+  //old key
+  //apiKey :"key_0e1554454e0f48479255a7a5be76bc74",
+  //apiToken :"tok_4740ac55f56e43f2ba2f302b138fcf81",
+
+  //new key
+  apiKey : "key_ba9cbb9b470f464d8f2b51fcd5f39a69",
+  apiToken : "tok_2b3c5b15c8664b32a3cb241607ffe3c5",
   contentLanguage:"en-US",
   chosenVoicePrintPhrase:"my face and voice identify me"
 }
